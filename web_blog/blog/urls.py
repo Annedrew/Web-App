66f6cdb353a06e55a27160818d3b126eac10a32b
Annedrew <1196244890@qq.com>
2024-01-11 09:38:18 +0100

    Filter by user name

--- web_blog/blog/urls.py
+++ web_blog/blog/urls.py
@@ -5,10 +5,12 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
